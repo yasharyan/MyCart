eb739bbb098575d9bbcc45c1cc3e349b4cc4bfa9
yasharyan <997yashraj@gmail.com>
2020-11-05 03:53:01 +0530

    Updates

---
+++ precache-manifest.104264e6d0ab1c67ab6aa55773dc55b7.js
@@ -0,0 +1,458 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa62f15decaa622a4a995f31e6a44cb",
+    "url": "/MyCart/index.html"
+  },
+  {
+    "revision": "e15612a323ffc92b370f",
+    "url": "/MyCart/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "04aa716753196a4cbb25",
+    "url": "/MyCart/static/css/main.6cb74e0c.chunk.css"
+  },
+  {
+    "revision": "e15612a323ffc92b370f",
+    "url": "/MyCart/static/js/2.77c28c98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MyCart/static/js/2.77c28c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04aa716753196a4cbb25",
+    "url": "/MyCart/static/js/main.9139ea35.chunk.js"
+  },
+  {
+    "revision": "5871c210e183b5ed01bd",
+    "url": "/MyCart/static/js/runtime-main.7674a8ee.js"
+  },
+  {
+    "revision": "571a4d6c2213179afb60eabe25a0989e",
+    "url": "/MyCart/static/media/Travelling Luggages.571a4d6c.jpg"
+  },
+  {
+    "revision": "4f01b4b17e34069cb264cff4d4ac2dda",
+    "url": "/MyCart/static/media/accessories1.4f01b4b1.jpg"
+  },
+  {
+    "revision": "82bc9c953df03e9846dcc3305fd0e59f",
+    "url": "/MyCart/static/media/accessories1.82bc9c95.jpg"
+  },
+  {
+    "revision": "711d25782e9e3bafcf199c04781abf29",
+    "url": "/MyCart/static/media/accessories2.711d2578.jpg"
+  },
+  {
+    "revision": "981998d553aa8547c907564c9a2de4db",
+    "url": "/MyCart/static/media/accessories3.981998d5.jpg"
+  },
+  {
+    "revision": "4a1cc294301e743f07a076d949d5623f",
+    "url": "/MyCart/static/media/accessories4.4a1cc294.jpg"
+  },
+  {
+    "revision": "09ee9a277ef0ec74a89faa818279546f",
+    "url": "/MyCart/static/media/appliances1.09ee9a27.jpg"
+  },
+  {
+    "revision": "24593f8c79baf63874bfb6b355e43fad",
+    "url": "/MyCart/static/media/appliances2.24593f8c.jpg"
+  },
+  {
+    "revision": "f032e92b0960b4325dae60c55aafb97e",
+    "url": "/MyCart/static/media/appliances3.f032e92b.jpg"
+  },
+  {
+    "revision": "a7bc3a4188e1ad44f8149d7304818815",
+    "url": "/MyCart/static/media/appliances4.a7bc3a41.jpg"
+  },
+  {
+    "revision": "48c967c2a77d9414c96f37b3a6bb7d5f",
+    "url": "/MyCart/static/media/books2.48c967c2.jpg"
+  },
+  {
+    "revision": "1913b57e8cfd6a7272289641c98d1036",
+    "url": "/MyCart/static/media/camera10edit.1913b57e.jpg"
+  },
+  {
+    "revision": "f1decc45630c27759e7482f33d681d40",
+    "url": "/MyCart/static/media/camera6.f1decc45.jpg"
+  },
+  {
+    "revision": "a53b2b26f574a9b8200e49dfbb8119d7",
+    "url": "/MyCart/static/media/electronics1.a53b2b26.jpg"
+  },
+  {
+    "revision": "c9876fee428dd31e92f61853896d5545",
+    "url": "/MyCart/static/media/electronics2.c9876fee.jpg"
+  },
+  {
+    "revision": "e39a951139513be5af044fcaa5fd205b",
+    "url": "/MyCart/static/media/electronics2.e39a9511.jpg"
+  },
+  {
+    "revision": "37eaaa048186e1476aacc2c81ea83e5b",
+    "url": "/MyCart/static/media/electronics3.37eaaa04.jpg"
+  },
+  {
+    "revision": "a850d09239404a012f3609aa633196c7",
+    "url": "/MyCart/static/media/electronics4.a850d092.jpg"
+  },
+  {
+    "revision": "7e659f4db5799e0505b54df12b75f925",
+    "url": "/MyCart/static/media/fourthareaproduct1.7e659f4d.jpg"
+  },
+  {
+    "revision": "da0011f52016764a915aa6b44bcad088",
+    "url": "/MyCart/static/media/fourthareaproduct2.da0011f5.jpg"
+  },
+  {
+    "revision": "6308f9bc8738dd76085ad42969ca7188",
+    "url": "/MyCart/static/media/fourthareaproduct3.6308f9bc.jpg"
+  },
+  {
+    "revision": "0be3535630c324ec208301894aa91ed3",
+    "url": "/MyCart/static/media/fourthareaproduct4.0be35356.jpg"
+  },
+  {
+    "revision": "b2f6a151eede487ce3a7417a1a720a8a",
+    "url": "/MyCart/static/media/furniture1.b2f6a151.jpg"
+  },
+  {
+    "revision": "46c59bb5769f4e42dc78a7806dda3024",
+    "url": "/MyCart/static/media/furniture2.46c59bb5.jpg"
+  },
+  {
+    "revision": "81b6c167dc1eb941c8bf37c5be0f07a6",
+    "url": "/MyCart/static/media/furniture2.81b6c167.jpg"
+  },
+  {
+    "revision": "df230e1be1c143d699cd4624a96c8ff8",
+    "url": "/MyCart/static/media/furniture3.df230e1b.jpg"
+  },
+  {
+    "revision": "3e31bb2dd40dd5ed076dceea37acb837",
+    "url": "/MyCart/static/media/furniture4.3e31bb2d.jpg"
+  },
+  {
+    "revision": "6d47d97c44a2b1f94280d0b58d43e214",
+    "url": "/MyCart/static/media/instrument1.6d47d97c.jpg"
+  },
+  {
+    "revision": "b7318d93f7f5adf4d2df45b1323d260c",
+    "url": "/MyCart/static/media/instrument2.b7318d93.jpg"
+  },
+  {
+    "revision": "23539d2641212585f559c8070798c2a5",
+    "url": "/MyCart/static/media/instrument3.23539d26.jpg"
+  },
+  {
+    "revision": "00f788b59365dcc51ba4f08a1aa18299",
+    "url": "/MyCart/static/media/instrument4.00f788b5.jpg"
+  },
+  {
+    "revision": "d244b0992e3c78b7d7bcf5f7f614d0d8",
+    "url": "/MyCart/static/media/iphone 11 banner1.d244b099.jpg"
+  },
+  {
+    "revision": "94f9b65c208e9bb29b15cf9e0bdec0ed",
+    "url": "/MyCart/static/media/iphone 11 banner2.94f9b65c.jpg"
+  },
+  {
+    "revision": "a30448fed545a505c16999202cffa2ef",
+    "url": "/MyCart/static/media/iphone 11 banner3.a30448fe.jpg"
+  },
+  {
+    "revision": "840cfb85e9bcac991187e2ce5aba17ce",
+    "url": "/MyCart/static/media/iphone 11 banner4.840cfb85.jpg"
+  },
+  {
+    "revision": "d695271fab95761b7160eeac36e3bfd1",
+    "url": "/MyCart/static/media/iphone 11 banner5.d695271f.jpg"
+  },
+  {
+    "revision": "4009e11856f69b2b56be4dc2ff2d1812",
+    "url": "/MyCart/static/media/iphone 11 banner6.4009e118.jpg"
+  },
+  {
+    "revision": "5cbe7b98826f5c38ad1ec7d0136e92a6",
+    "url": "/MyCart/static/media/iphone 11 banner7.5cbe7b98.jpg"
+  },
+  {
+    "revision": "6ac56614e440034350ee46848b5df3e4",
+    "url": "/MyCart/static/media/iphone 11.6ac56614.jpg"
+  },
+  {
+    "revision": "d30846995c46d6633274a2ce280e494c",
+    "url": "/MyCart/static/media/iphone 12 banner1.d3084699.jpg"
+  },
+  {
+    "revision": "7973967c5217eb9b2f60112b3bedc219",
+    "url": "/MyCart/static/media/iphone 12 banner2.7973967c.jpg"
+  },
+  {
+    "revision": "70000bd439f95eda575e66d12ac7e049",
+    "url": "/MyCart/static/media/iphone 12 banner3.70000bd4.jpg"
+  },
+  {
+    "revision": "a0f7a37db0838406140a2af6676dba44",
+    "url": "/MyCart/static/media/iphone 12 banner4.a0f7a37d.jpg"
+  },
+  {
+    "revision": "7b26b34dfa07e9c4af37531dafa6e7a9",
+    "url": "/MyCart/static/media/iphone 12 banner5.7b26b34d.jpg"
+  },
+  {
+    "revision": "5ef03ea1a2c941e5979176711a835458",
+    "url": "/MyCart/static/media/iphone 12 pro max banner1.5ef03ea1.jpg"
+  },
+  {
+    "revision": "7ccfaa1a4ffddf7da002fbcfff7ba071",
+    "url": "/MyCart/static/media/iphone 12 pro max banner2.7ccfaa1a.jpg"
+  },
+  {
+    "revision": "2fabfcca54a422a85e81896ebaf33e99",
+    "url": "/MyCart/static/media/iphone 12 pro max banner3.2fabfcca.jpg"
+  },
+  {
+    "revision": "cd593ae10e91c5657532b62ddc4e3150",
+    "url": "/MyCart/static/media/iphone 12 pro max banner4.cd593ae1.jpg"
+  },
+  {
+    "revision": "aacb6aaa758aa283d4f8dad398d38435",
+    "url": "/MyCart/static/media/iphone 12 pro max banner5.aacb6aaa.jpg"
+  },
+  {
+    "revision": "19f63b24e8bec94151e68e91855285d2",
+    "url": "/MyCart/static/media/iphone 12 pro max banner6.19f63b24.jpg"
+  },
+  {
+    "revision": "94c6547540d21bf44332c1e3a7c66a04",
+    "url": "/MyCart/static/media/iphone 12 pro.94c65475.jpg"
+  },
+  {
+    "revision": "571538e030acf82cf1ab1532a28cbfe5",
+    "url": "/MyCart/static/media/iphone 12.571538e0.jpg"
+  },
+  {
+    "revision": "7fe75447da42fe75a666a9a886946b7f",
+    "url": "/MyCart/static/media/iphone XR banner1.7fe75447.jpg"
+  },
+  {
+    "revision": "36cfd6671bb9653fc74f77425a67682f",
+    "url": "/MyCart/static/media/iphone XR banner2.36cfd667.jpg"
+  },
+  {
+    "revision": "9abe4cd03b08df0d47cc50064422d006",
+    "url": "/MyCart/static/media/iphone XR banner3.9abe4cd0.jpg"
+  },
+  {
+    "revision": "e681b14f0ec5c40be6a2903542a78714",
+    "url": "/MyCart/static/media/iphone XR banner4.e681b14f.jpg"
+  },
+  {
+    "revision": "a68b88efe94f21b1bec13410bb71fea1",
+    "url": "/MyCart/static/media/iphone XR.a68b88ef.jpg"
+  },
+  {
+    "revision": "f321144e11be144d775c268c26b04258",
+    "url": "/MyCart/static/media/jeremy-alford-68_PLKkF_ww-unsplash.f321144e.jpg"
+  },
+  {
+    "revision": "08caa71dddffb807bac00c8494dc8862",
+    "url": "/MyCart/static/media/jonas-lee-o6elTKWZ5bI-unsplash.08caa71d.jpg"
+  },
+  {
+    "revision": "5f4a56ffb33256fa4572e62bb4807b05",
+    "url": "/MyCart/static/media/kid1.5f4a56ff.jpg"
+  },
+  {
+    "revision": "241c73f10aefcc8c16300675fc9bd07f",
+    "url": "/MyCart/static/media/kid2.241c73f1.jpg"
+  },
+  {
+    "revision": "75ead5b056f82242bc0c7dc7f11d76c3",
+    "url": "/MyCart/static/media/kid3.75ead5b0.jpg"
+  },
+  {
+    "revision": "27b6341d1e9e1835b63fa8fe41e40983",
+    "url": "/MyCart/static/media/kid4.27b6341d.jpg"
+  },
+  {
+    "revision": "46c28cb086817067ff57a0aed3c2e994",
+    "url": "/MyCart/static/media/kids2.46c28cb0.jpg"
+  },
+  {
+    "revision": "0be4616f6af5db30a87de1f170f998cc",
+    "url": "/MyCart/static/media/lan-deng-quddu_dZKkQ-unsplash.0be4616f.jpg"
+  },
+  {
+    "revision": "8948f0b967fa566d39ae2f037dc32c1f",
+    "url": "/MyCart/static/media/laptop1.8948f0b9.jpg"
+  },
+  {
+    "revision": "f82964731839bdabd1dd787fa5908df1",
+    "url": "/MyCart/static/media/laptop1.f8296473.jpg"
+  },
+  {
+    "revision": "1f8ba240e50a2e606bbfeabb8885ed0f",
+    "url": "/MyCart/static/media/laptop2.1f8ba240.jpg"
+  },
+  {
+    "revision": "cf2c707b9a40dd5c7d8ebe5cf0e3ce14",
+    "url": "/MyCart/static/media/laptop3.cf2c707b.jpg"
+  },
+  {
+    "revision": "fa5528d4059f4a95757538cba52105af",
+    "url": "/MyCart/static/media/laptop4.fa5528d4.jpg"
+  },
+  {
+    "revision": "34876a62a1122068c2406aa688521bd3",
+    "url": "/MyCart/static/media/luggages1.34876a62.jpg"
+  },
+  {
+    "revision": "82da006d17e1b6152ab0a6e9c117a314",
+    "url": "/MyCart/static/media/luggages2.82da006d.jpg"
+  },
+  {
+    "revision": "af6b22dd11235f2815b04851de93e831",
+    "url": "/MyCart/static/media/luggages3.af6b22dd.jpg"
+  },
+  {
+    "revision": "a72bda70297a776d0e826ee936e89335",
+    "url": "/MyCart/static/media/luggages4.a72bda70.jpg"
+  },
+  {
+    "revision": "3d6284d0c4f81a2c5e895b04e4179d9d",
+    "url": "/MyCart/static/media/mnz-RsJDUzKdBws-unsplash.3d6284d0.jpg"
+  },
+  {
+    "revision": "d43497f41419ae2a64f2264d52089f5c",
+    "url": "/MyCart/static/media/mobile2.d43497f4.jpg"
+  },
+  {
+    "revision": "6cb747e984e1e5817a55153be26181b8",
+    "url": "/MyCart/static/media/mobile6.6cb747e9.jpg"
+  },
+  {
+    "revision": "371970d3d64499387b471d450b541acc",
+    "url": "/MyCart/static/media/musics2.371970d3.jpg"
+  },
+  {
+    "revision": "5b3694165eb9667e0aac2f8174425015",
+    "url": "/MyCart/static/media/orderbox.5b369416.png"
+  },
+  {
+    "revision": "b034cc0b353374f46cc3221496236772",
+    "url": "/MyCart/static/media/sajad-nori-uRIil5LFBaI-unsplash.b034cc0b.jpg"
+  },
+  {
+    "revision": "1058c502bf31b7c9504d940589d549ac",
+    "url": "/MyCart/static/media/secondareaproduct1.1058c502.jpg"
+  },
+  {
+    "revision": "14999e74f29ee01a8dd2f5a30d27062e",
+    "url": "/MyCart/static/media/secondareaproduct2.14999e74.jpg"
+  },
+  {
+    "revision": "3f095814c261e7f7178a389a2e548667",
+    "url": "/MyCart/static/media/secondareaproduct3.3f095814.jpg"
+  },
+  {
+    "revision": "aa026d2275fb2319c65ffd00135ad685",
+    "url": "/MyCart/static/media/secondareaproduct4.aa026d22.jpg"
+  },
+  {
+    "revision": "f97ffa73613e86bd873263b6f1cf317e",
+    "url": "/MyCart/static/media/shoes4.f97ffa73.jpg"
+  },
+  {
+    "revision": "218f17248cc60581787145a136dbcded",
+    "url": "/MyCart/static/media/sonyproduct1banner.218f1724.jpg"
+  },
+  {
+    "revision": "78102880bec809e451a77aeab5df1ccc",
+    "url": "/MyCart/static/media/sonyproduct1banner2.78102880.jpg"
+  },
+  {
+    "revision": "0247242c0415fc8b2a0235e16f322d75",
+    "url": "/MyCart/static/media/sonyproduct2banner.0247242c.jpg"
+  },
+  {
+    "revision": "6dc292375acd8b472964fa2b75896da9",
+    "url": "/MyCart/static/media/sonyproduct3banner.6dc29237.jpg"
+  },
+  {
+    "revision": "5f5c62f99821ea83d65c2909873f33a8",
+    "url": "/MyCart/static/media/sonyproduct4banner.5f5c62f9.jpg"
+  },
+  {
+    "revision": "cbd900db6a32fb1f2e9b9fe06b32a2db",
+    "url": "/MyCart/static/media/sports1.cbd900db.jpg"
+  },
+  {
+    "revision": "9fb7c9f4b411a066a2becdf41ffa112e",
+    "url": "/MyCart/static/media/sports2.9fb7c9f4.jpg"
+  },
+  {
+    "revision": "76f9a526ae7883d4984db8d5475c0075",
+    "url": "/MyCart/static/media/sports3.76f9a526.jpg"
+  },
+  {
+    "revision": "59399940dff821c347bbe796e7d00ef7",
+    "url": "/MyCart/static/media/sports4.59399940.jpg"
+  },
+  {
+    "revision": "8cf6bafc1501a710c65300e77ff6327f",
+    "url": "/MyCart/static/media/thirdareaproduct1.8cf6bafc.jpg"
+  },
+  {
+    "revision": "0615180aa92118fb77df02d22c5f95fe",
+    "url": "/MyCart/static/media/thirdareaproduct2.0615180a.jpg"
+  },
+  {
+    "revision": "aba2a1e4f381fa564b9deb4540a411f8",
+    "url": "/MyCart/static/media/thirdareaproduct3.aba2a1e4.jpg"
+  },
+  {
+    "revision": "f95af7706f439982e0bd06622ca4533b",
+    "url": "/MyCart/static/media/thirdareaproduct4.f95af770.jpg"
+  },
+  {
+    "revision": "045af90d76b354674f04b0156a1fea17",
+    "url": "/MyCart/static/media/watch1.045af90d.jpg"
+  },
+  {
+    "revision": "31303d5ffe563314315426a5c3a4c59b",
+    "url": "/MyCart/static/media/watch2.31303d5f.jpg"
+  },
+  {
+    "revision": "29d8948e1f8ee24f4bd06163e4abda07",
+    "url": "/MyCart/static/media/watch3.29d8948e.jpg"
+  },
+  {
+    "revision": "ececfed9772551d22a2da635244cfdc6",
+    "url": "/MyCart/static/media/watch3.ececfed9.jpg"
+  },
+  {
+    "revision": "cd23a26dddf215430951fd77fae2507b",
+    "url": "/MyCart/static/media/watch4.cd23a26d.jpg"
+  },
+  {
+    "revision": "cf0814ee6e2e134a5ab46d52aa4daa58",
+    "url": "/MyCart/static/media/womens1.cf0814ee.jpg"
+  },
+  {
+    "revision": "054de7f496ac3dfaca32e89843b4f5f0",
+    "url": "/MyCart/static/media/womens2.054de7f4.jpg"
+  },
+  {
+    "revision": "0c2c9bf32f908e253e76032c813a1430",
+    "url": "/MyCart/static/media/womens3.0c2c9bf3.jpg"
+  },
+  {
+    "revision": "896dab325afa6e8a8d6cf3bf8920b77c",
+    "url": "/MyCart/static/media/womens4.896dab32.jpg"
+  }
+]);
